0c853ce6f2199f662e62658e4e807f19d74101d8
Colin Cason <colinjcason@gmail.com>
2020-10-18 19:49:43 -0700

    created header, footer, homescreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Container } from 'react-bootstrap';
+import HomeScreen from './screens/HomeScreen';
 
 const App = () => {
   return (
     <>
-     <h1>Welcome to ProShop</h1> 
+    <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to ProShop</h1>
+          <HomeScreen />
+        </Container>        
+      </main> 
+     <Footer />
     </>
   );
 }
