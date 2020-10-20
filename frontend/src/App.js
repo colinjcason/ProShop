9f9d79669613715e2964e8439658be822097722f
Colin Cason <colinjcason@gmail.com>
2020-10-19 20:46:45 -0700

    created product page and finsihed products card display

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { Container } from 'react-bootstrap';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
   return (
-    <>
+    <Router>
     <Header />
       <main className='py-3'>
         <Container>
-          <h1>Welcome to ProShop</h1>
-          <HomeScreen />
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
         </Container>        
       </main> 
      <Footer />
-    </>
+    </Router>
   );
 }
 
