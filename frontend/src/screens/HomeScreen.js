52de3397a11f5197c4eac8a414a6f61343100bf1
Colin Cason <colinjcason@gmail.com>
2020-10-23 20:06:11 -0700

    created loading spinner and error message components

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
 import { listProducts } from '../actions/productActions';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
 
 const HomeScreen = () => {
     const dispatch = useDispatch();
@@ -17,9 +19,9 @@ const HomeScreen = () => {
         <>
             <h1>Latest Products</h1>
             {loading ? 
-                <h2>Loading...</h2> 
+                <Loader />
                 : error ? 
-                <h3>{error}</h3> 
+                <Message variant='danger'>{error}</Message> 
                 : 
                 <Row>
                 {products.map(product => (
