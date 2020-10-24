1bcd2543060759a35970f7031081f21f22e2ac8e
Colin Cason <colinjcason@gmail.com>
2020-10-24 12:04:48 -0700

    created product page redux action, reducer, constant and api request

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,4 +1,10 @@
-import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL } from '../constants/productConstants';
+import { PRODUCT_LIST_REQUEST, 
+    PRODUCT_LIST_SUCCESS, 
+    PRODUCT_LIST_FAIL, 
+    PRODUCT_DETAILS_REQUEST, 
+    PRODUCT_DETAILS_SUCCESS, 
+    PRODUCT_DETAILS_FAIL } from '../constants/productConstants';
+
 import axios from 'axios';
 
 export const listProducts = () => async (dispatch) => {
@@ -14,6 +20,24 @@ export const listProducts = () => async (dispatch) => {
     } catch (error) {
         dispatch({
             type: PRODUCT_LIST_FAIL,
+            payload: error.response && error.response.data.message ? error.response.data.message : error.message
+        })
+    }
+}
+
+export const listProductDetails = (id) => async (dispatch) => {
+    try {
+        dispatch({ type: PRODUCT_DETAILS_REQUEST })
+
+        const { data } = await axios.get(`/api/products/${id}`)
+
+        dispatch({
+            type: PRODUCT_DETAILS_SUCCESS,
+            payload: data,
+        })
+    } catch (error) {
+        dispatch({
+            type: PRODUCT_DETAILS_FAIL,
             payload: error.response && error.response.data.message ? error.response.data.message : error.message
         })
     }
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -2,25 +2,26 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from '../components/Rating';
-import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
+import { listProductDetails } from '../actions/productActions';
+import Message from '../components/Message'
+import Loader from '../components/Loader'
 
 const ProductScreen = ({ match }) => {
-    const [product, setProduct] = useState({});
+    const dispatch = useDispatch();
+
+    const productDetails = useSelector(state => state.productDetails)
+    const { loading, error, product } = productDetails
 
     useEffect(() => {
-        const fetchProduct = async () => {
-            const { data } = await axios.get(`/api/products/${match.params.id}`);
-
-            setProduct(data);
-        }
-
-        fetchProduct();
-    }, [match])
+        dispatch(listProductDetails(match.params.id))
+    }, [dispatch, match])
 
     return (
         <>
             <Link className='btn btn-light my-3' to='/'>Go Back</Link>
-            <Row>
+            {loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
+                <Row>
                 <Col md={6}>
                     <Image src={product.image} alt={product.name} fluid/>
                 </Col>
@@ -68,6 +69,7 @@ const ProductScreen = ({ match }) => {
                     </Card>
                 </Col>
             </Row>
+            )}
         </>
     )
 }
