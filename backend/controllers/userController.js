e075e75446662d8b93c6b6b5daa8ac28f3abde96
Colin Cason <colinjcason@gmail.com>
2020-10-28 22:09:06 -0700

    created user profile screen

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -53,7 +53,7 @@ const registerUser = asyncHandler(async (req, res) => {
     }
 })
 
-// get user by id
+// get user profile
 const getUserProfile = asyncHandler(async (req, res) => {
     const user = await User.findById(req.user._id)
 
@@ -66,8 +66,35 @@ const getUserProfile = asyncHandler(async (req, res) => {
         })
     } else {
         res.status(404)
+        throw new Error('cant find' + req.user._id)
+    }
+})
+
+// update user profile
+// PUT /api/users/profile
+const updateUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id)
+
+    if(user) {
+        user.name = req.body.name || user.name
+        user.email = req.body.email || user.email
+        if(req.body.password) {
+            user.password = req.body.password
+        }
+
+        const updatedUser = await user.save()
+        
+        res.json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin,
+            token: generateToken(updatedUser._id)
+        })
+    } else {
+        res.status(404)
         throw new Error('User not found')
     }
 })
 
-export { authUser, getUserProfile, registerUser }
+export { authUser, getUserProfile, registerUser, updateUserProfile }
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,7 +4,9 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import { productListReducer, 
     productDetailsReducer, } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers'
-import { userLoginReducer, userRegisterReducer } from './reducers/userReducers'
+import { userLoginReducer, 
+    userRegisterReducer, 
+    userDetailsReducer } from './reducers/userReducers'
 
 
 const reducer = combineReducers({
@@ -12,7 +14,8 @@ const reducer = combineReducers({
     productDetails: productDetailsReducer,
     cart: cartReducer,
     userLogin: userLoginReducer,
-    userRegister: userRegisterReducer
+    userRegister: userRegisterReducer,
+    userDetails: userDetailsReducer
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
