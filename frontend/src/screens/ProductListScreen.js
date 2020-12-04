eff14334e6828c2c89fe53696833c052bc1c6fb8
Colin Cason <colinjcason@gmail.com>
2020-12-03 17:43:02 -0800

    created redux files for Admin to create new product

--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -4,7 +4,8 @@ import { Table, Button, Row, Col } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
-import { listProducts, deleteProduct } from '../actions/productActions'
+import { listProducts, deleteProduct, createProduct } from '../actions/productActions'
+import { PRODUCT_CREATE_RESET } from '../constants/productConstants'
 
 const ProductListScreen = ({ history, match }) => {
     const dispatch = useDispatch()
@@ -18,13 +19,26 @@ const ProductListScreen = ({ history, match }) => {
     const productDelete = useSelector(state => state.productDelete)
     const { loading:loadingDelete, success:successDelete, error: errorDelete } = productDelete
 
+    const productCreate = useSelector(state => state.productCreate)
+    const { 
+        loading:loadingCreate, 
+        success:successCreate, 
+        error: errorCreate,
+        product: createdProduct
+     } = productCreate
+
     useEffect(() => {
-        if(userInfo && userInfo.isAdmin) {
-            dispatch(listProducts())
-        } else {
+        dispatch({ type: PRODUCT_CREATE_RESET })
+        if(!userInfo.isAdmin) {
             history.push('/login')
         }
-    }, [dispatch, history, userInfo, successDelete])
+
+        if(successCreate) {
+            history.push(`/admin/products/${createdProduct._id}/edit`)
+        } else {
+            dispatch(listProducts())
+        }
+    }, [dispatch, history, userInfo, successDelete, successCreate, createdProduct])
 
     const deleteProductHandler = (id) => {
         if(window.confirm('Are you sure you want to delete this product?')) {
@@ -33,7 +47,7 @@ const ProductListScreen = ({ history, match }) => {
     }
 
     const createProductHandler = () => {
-
+        dispatch(createProduct())
     }
 
     return (
@@ -50,6 +64,8 @@ const ProductListScreen = ({ history, match }) => {
             </Row>
             {loadingDelete && <Loader />}
             {errorDelete && <Message variant='danger'>{errorDelete}</Message>}
+            {loadingCreate && <Loader />}
+            {errorCreate && <Message variant='danger'>{errorCreate}</Message>}
             {loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> 
             : (
                 <Table striped bordered hover responsive className='table-sm'>
