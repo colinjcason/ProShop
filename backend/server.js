f0a0e91ddf8cf69219af2ecc4a96ac7a2ebb3ab6
Colin Cason <colinjcason@gmail.com>
2020-12-09 17:45:02 -0800

    created route for uploading image file to create new product using Multer

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,11 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import colors from 'colors';
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
-
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 
 dotenv.config();
 
@@ -20,11 +20,11 @@ app.use(express.json())
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 
 app.get('/api/config/paypal', (req, res) => 
   res.send(process.env.PAYPAL_CLIENT_ID)
 )
-
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
